fea1f0e56f0f87205c6618f3f0cc23d3fb5b1521
Mario Di Vece <mario@unosquare.com>
2021-01-08 10:38:25 -0600

    rollback to tubular 2.2

--- BlazorificSample.Server/Startup.cs
+++ BlazorificSample.Server/Startup.cs
@@ -19,9 +19,14 @@ namespace BlazorificSample.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            // services.AddResponseCompression(opts =>
+            // {
+            //     opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+            //         new[] { "application/octet-stream" });
+            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
